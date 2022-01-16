c1c5d05cf8ec36f7890356ae28a0c71ee70a7a8b
Lartey Joshua <larteyjoshua@gmail.com>
2022-01-16 12:51:34 +0000

    LAUNCHAPP-24 Schemas Added

--- Backend/utils/config.py
+++ Backend/utils/config.py
@@ -7,23 +7,23 @@ from pydantic import BaseSettings, PostgresDsn, validator
 class Settings(BaseSettings):
     PROJECT_NAME: str = "LaunchApp"
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    SECRET_KEY: str = ""
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    ALGORITHM:str = "HS256"
+    ALGORITHM:str = ""
     USERS_OPEN_REGISTRATION: Optional[int] = None
     ALLOWED_EXTENSIONS = ('png', 'jpg', 'jpeg') 
     ENVIRONMENT: Optional[str]
 
-    FIRST_SUPER_ADMIN_EMAIL: str = "superuser@launchapp.com"
-    FIRST_SUPER_ADMIN_PASSWORD: str  = "superpassword"
+    FIRST_SUPER_ADMIN_EMAIL: str = ""
+    FIRST_SUPER_ADMIN_PASSWORD: str  = ""
     
-    AWS_ACCESS_KEY_ID: str='AKIA3ILV3U7F2Z3QLRES'
-    AWS_SECRET_ACCESS_KEY="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"
+    AWS_ACCESS_KEY_ID: str=''
+    AWS_SECRET_ACCESS_KEY=""
     REGION_NAME: str = 'us-east-2'
     SIGNATURE_VERSION: str='s3v4'
     BUCKET_NAME: str ="launchapp-images"
 
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:12345678@localhost:5432/launchapp"
+    SQLALCHEMY_DATABASE_URI: str = ""
 
     class Config:
         case_sensitive = True
